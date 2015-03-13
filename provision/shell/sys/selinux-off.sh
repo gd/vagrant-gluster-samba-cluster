420577afac3d162c3dcedb204b74d62e013046e0
Michael Adam <obnox@samba.org>
2015-03-13 01:34:45 +0100

    Add informative messages to the selinux script

--- provision/shell/sys/selinux-off.sh
+++ provision/shell/sys/selinux-off.sh
@@ -6,12 +6,16 @@ set -e
 
 GETENFORCE="$(getenforce 2> /dev/null)"
 
-[ "${GETENFORCE}" = "Disabled" ] || {
+[ "${GETENFORCE}" = "Disabled" ] && {
+  echo "Selinux already disabled."
+} || {
+  echo "Setting selinux policy to permissive for this session."
   setenforce permissive
 }
 
 BACKUP_SUFFIX=".orig.$(date +%Y%m%d-%H%M%S)"
 
+echo "Disabling selinux permanently."
 FILE=/etc/selinux/config
 test -f ${FILE} && {
   sed -i${BACKUP_SUFFIX} -e 's/^SELINUX=.*$/SELINUX=disabled/g' ${FILE}
